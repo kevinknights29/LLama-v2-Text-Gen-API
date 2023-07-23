03a84df76ae80c3dc1e7f9017c45ffb7e75a96c9
Kevin Knights <kevinknights29@hotmail.com>
2023-07-23 08:55:48 -0500

    added text generation GET method

---
+++ main.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from flask import Flask
+
+from src.llm import text_generation
+
+app = Flask(__name__)
+
+
+@app.get("/generate-text/<string:prompt>")
+def generate_text(prompt):
+    result = text_generation(prompt)
+    return {"result": result}
