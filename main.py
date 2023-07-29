085adc637477b096a3e8c21befdf616e8be8794c
Kevin Knights <kevinknights29@hotmail.com>
2023-07-28 22:02:18 -0500

    WIP: added latest openapi & swagger-ui versions

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["API_TITLE"] = "Text Generation REST API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.1.0"
-app.config["OPENAPI_URL_PREFIX"] = "/api"
+app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger"
-app.config["OPENAPI_SWAGGER_UI_VERSION"] = "5.2.0"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config["OPENAPI_SWAGGER_UI_VERSION"] = "5.2.0"
 
 
 api = Api(app)
